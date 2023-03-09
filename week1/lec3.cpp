b0d799957ef47d499af6454c435b94644acdd890
Ganesh Darewar <71462162+ganeshdarewar@users.noreply.github.com>
2023-03-09 21:56:36 +0530

    week1

---
+++ week1/lec3.cpp
@@ -0,0 +1,142 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//     //1. Continals 
+//     int marks;
+//     cout<<"Enter marks:"<<endl;
+//     cin>>marks;
+//     if(marks>=90){
+//         cout<<"A Grade";
+//     }
+//     else if(marks >=50){
+//         cout<<"B Grade";
+//     }
+    
+//     // else{ // Else is not neceassary
+//     //     cout<<"failed";
+//     // }
+    
+
+
+    // 2. Loops
+    // //2 table
+    // for(int i=1; i<=10;i++){
+    //     cout<<2*i<<endl;
+    // }
+    // // full table
+    // int n;
+    // cout<<"Enter table to print";
+    // cin>>n;
+    // for(int i=1; i<=10;i++){
+    //     cout<<n*i<<endl;
+    // }
+
+    // for(int i=100; i<10;i=i/2){
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=5; (i<10 && i<10); i++){
+    //     cout<<i;
+    // }
+
+    // int n;
+    // if(cin>>n){
+    //     cout<<"ganesh";
+    // }
+    //  int n;
+    // if(cout<<n){
+    //     cout<<"ganesh";
+    // }
+
+
+    //3. patterns
+    //solid pattern
+    // int nrow, ncol;
+    // cin>>nrow>>ncol;
+    // for (int row = 0; row < nrow; row++)
+    // {
+    //     for (int col = 0; col <ncol ; col++)
+    //     {
+    //         /* code */
+    //         cout<<" * ";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+    // //Hollow rectangle
+    // int nrow, ncol ;
+    // cin>>nrow>>ncol;
+    // for (int row = 0; row < nrow; row++)
+    // {
+    //     if(row==0 || row==nrow-1){
+    //         for(int col=0; col<ncol;col++){
+    //             cout<<"*";
+    //         }
+    //     }    
+    //     else{
+    //         cout<<"*";
+    //         for(int i=0; i<ncol-2; i++ ){
+    //             cout<<" ";
+    //         }
+    //         cout<<"*";
+    //     }    
+    //     cout<<endl;
+    // }
+    
+  //Half Pyramid
+    // int nrow;
+    // cin>>nrow;
+    // for (int row = 0; row < nrow; row++)
+    // {
+    //     /* code */
+    //     for (int col = 0; col < row+1; col++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+
+    // //Inverted Half Pyramid
+    // int nrow;
+    // cin>>nrow;
+    // for (int row = 0; row < nrow; row++)
+    // {
+    //     for (int col = 0; col <nrow-row; col++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+    // Numeric Half Pyramid
+    // int nrow;
+    // cin>>nrow;
+    // for (int row = 0; row < nrow; row++)
+    // {
+    //     for (int col = 0; col < row+1; col++)
+    //     {
+    //         cout<<col+1;
+    //     }
+    //     cout<<endl;
+        
+    // }
+    //  int nrow;
+    // cin>>nrow;
+    // for (int row = 0; row < nrow; row++)
+    // {
+    //     for (int col = 0; col < nrow-row; col++)
+    //     {
+    //         cout<<col+1;
+    //     }
+    //     cout<<endl;
+        
+    // }
+    
+    return 0;
+}
---
+++ week1/lecture2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int arr[]={1,2,3,4};
+    cout<<arr[1];
+    return 0;
+}
